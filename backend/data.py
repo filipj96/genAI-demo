a944c9ee6ff2f861666388dbc97b114388e9f4e3
Simon Shadman <simon.shadman@gmail.com>
2023-06-13 14:56:55 +0200

    SimpleDatastore search now returns a list of tuples

--- backend/data.py
+++ backend/data.py
@@ -9,14 +9,12 @@ class SimpleDataStore():
         self.df = pd.read_csv(self.datafile_path)
         self.df["embedding"] = self.df.embedding.apply(eval).apply(array)
 
-    def search(self, embedding: list[float], n: int = 5) -> list[str]:
+    def search(self, embedding: list[float], n: int = 5) -> list[zip]:
         self.df["similarity"] = self.df.embedding.apply(
             lambda x: cosine_similarity(x, embedding))
-        results_series = (
-            self.df.sort_values("similarity", ascending=False)
-            .head(n)["combined"]
-        )
-        return results_series.tolist()
+        results_df = self.df.sort_values("similarity", ascending=False).head(n)
+
+        return list(zip(results_df["name"], results_df["combined"]))
 
 if __name__ == '__main__':
     import os
@@ -38,6 +36,6 @@ if __name__ == '__main__':
     # Test with comma separated keywords
     #comma_separated_keywords = "red wine, fruity, lamb, Italy"
     #embedding = get_embedding(comma_separated_keywords, engine="text-embedding-ada-002")
-    #top_10_similar = d.search(embedding=embedding, n=10)
+    #top_10_similar = d.search(embedding=embedding, n=5)
     #for t in top_10_similar:
-    #    print(t)
+    #    print("Namn: " + t[0] + "; " + t[1])
